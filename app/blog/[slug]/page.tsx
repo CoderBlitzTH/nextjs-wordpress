a415a904274aa1f3aeb6e356876f30f854f27d64
Aphagon <aphagon@gmail.com>
2025-02-26 01:06:27 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,12 +1,51 @@
+import { Metadata } from 'next';
+
 import { BlogPost } from '@/components/ui/blog';
 import { GetPostDocument } from '@/graphql/generated/graphql';
 import { query } from '@/lib/apolloClient';
-import type { PageProps } from '@/types/page';
+import config from '@/lib/config';
+import type { PageProps } from '@/types';
 
-export default async function Page({ params }: Readonly<PageProps>) {
-  // Get the slug from the params.
+/**
+ * Generate the static routes at build time.
+ *
+ * @see https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+ */
+export function generateStaticParams() {
+  return [];
+}
+
+/**
+ * Generate the metadata for each static route at build time.
+ *
+ * @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#generatemetadata-function
+ */
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
   const { slug } = await params;
+  const { data } = await query({
+    query: GetPostDocument,
+    variables: { slug },
+  });
 
+  const post = data?.post;
+
+  if (!post) return {};
+
+  return {
+    title: `${post.title} - ${config.siteName}`,
+    description: post.excerpt,
+  };
+}
+
+/**
+ * The blog post route.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#pages
+ */
+export default async function BlogPostPage({ params }: Readonly<PageProps>) {
+  const { slug } = await params;
   const {
     data: { post },
   } = await query({
--- app/blog/category/[slug]/page.tsx
+++ app/blog/category/[slug]/page.tsx
@@ -1,29 +1,60 @@
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 import { BlogList } from '@/components/ui/blog';
 import type { GetPostsByCategoryQuery } from '@/graphql/generated/graphql';
 import { GetPostsByCategoryDocument } from '@/graphql/generated/graphql';
 import { query } from '@/lib/apolloClient';
-import { PageProps } from '@/types/page';
-
-export default async function Page({ params }: Readonly<PageProps>) {
-  // Get the slug from the params.
+import config from '@/lib/config';
+import type { PageProps } from '@/types';
+/**
+ * Generate the metadata for each static route at build time.
+ *
+ * @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#generatemetadata-function
+ */
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
   const { slug } = await params;
   const { data } = await query<GetPostsByCategoryQuery>({
     query: GetPostsByCategoryDocument,
     variables: { first: 10, slug },
   });
 
-  if (!data?.category || !data?.category?.posts?.nodes) {
+  const { category } = data;
+
+  if (!category || !category?.posts?.nodes) {
+    return {};
+  }
+
+  return {
+    title: `${category.name} - ${config.siteName}`,
+    description: `หมวดหมู่ ${category.name}`,
+  };
+}
+
+/**
+ * The archive page route.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#pages
+ */
+export default async function Page({ params }: Readonly<PageProps>) {
+  const { slug } = await params;
+  const { data } = await query<GetPostsByCategoryQuery>({
+    query: GetPostsByCategoryDocument,
+    variables: { first: 10, slug },
+  });
+
+  const { category } = data;
+
+  if (!category || !category?.posts?.nodes) {
     notFound();
   }
 
   return (
     <>
-      <h1 className="mb-4 text-3xl font-bold">
-        หมวดหมู่: {data.category.name}
-      </h1>
-      <BlogList posts={data.category.posts.nodes} />
+      <h1 className="mb-4 text-3xl font-bold">{category.name}</h1>
+      <BlogList posts={category.posts.nodes} />
     </>
   );
 }
